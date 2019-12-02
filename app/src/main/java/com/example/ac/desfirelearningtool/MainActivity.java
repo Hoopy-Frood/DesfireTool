182ecb4d52db48884cab7d33b52d8199f6bd4c2a
Andrew Chan <andrew.chan@acs.com.hk>
2019-12-02 18:58:09 +0800

    Adding fChangeFileSettings function

--- app/src/main/java/com/example/ac/desfirelearningtool/MainActivity.java
+++ app/src/main/java/com/example/ac/desfirelearningtool/MainActivity.java
@@ -577,14 +577,18 @@ public class MainActivity extends AppCompatActivity implements IMainActivityCall
 
     //region Get Key Settings
     public void onGetKeySettings() {
+        onFragmentGetKeySettings();
+    }
 
+    public byte[] onFragmentGetKeySettings() {
+        Desfire.DesfireResponse res;
         try {
             scrollLog.appendTitle("Get Key Settings");
-            Desfire.DesfireResponse res = desfireCard.getKeySettings();
+            res = desfireCard.getKeySettings();
 
             if (res.status != Desfire.statusType.SUCCESS) {
                 scrollLog.appendError("Get Key Settings Failed: " + Desfire.DesfireErrorMsg(res.status));
-                return;
+                return null;
             }
             scrollLog.appendStatus("Key Settings: " + ByteArray.byteArrayToHexString(res.data));
 
@@ -633,57 +637,6 @@ public class MainActivity extends AppCompatActivity implements IMainActivityCall
             commandFragment.disableAllButtons();
             scrollLog.appendError("DESFire Disconnected\n");
             Log.e("onGetKeySettings", e.getMessage(), e);
-        }
-    }
-
-    public byte[] onFragmentGetKeySettings() {
-        Desfire.DesfireResponse res;
-        try {
-            scrollLog.appendTitle("Get Key Settings");
-            res = desfireCard.getKeySettings();
-
-            if (res.status != Desfire.statusType.SUCCESS) {
-                scrollLog.appendError("Error: " + Desfire.DesfireErrorMsg(res.status));
-                return null;
-            }
-            scrollLog.appendStatus("Key Settings: " + ByteArray.byteArrayToHexString(res.data));
-
-            if ((res.data[0] & (byte) 0xF0) == (byte) 0x00) {
-                scrollLog.appendData("- Change key access: Master Key");
-            } else if ((res.data[0] & (byte) 0xF0) == (byte) 0xE0) {
-                scrollLog.appendData("- Change key access right: Same Key");
-            } else if ((res.data[0] & (byte) 0xF0) == (byte) 0xF0) {
-                scrollLog.appendData("- Change key access right: Frozen");
-            } else {
-                scrollLog.appendData("Change key access right: Key " + ByteArray.byteToHexString((byte) (res.data[0] >> 4)));
-            }
-            if ((res.data[0] & (byte) 0x08) != (byte) 0x00) {
-                scrollLog.appendData("- Configuration changeable");
-            }
-            if ((res.data[0] & (byte) 0x04) != (byte) 0x00) {
-                scrollLog.appendData("- Master key not required for create/delete");
-            }
-            if ((res.data[0] & (byte) 0x02) != (byte) 0x00) {
-                scrollLog.appendData("- Free directory list access");
-            }
-            if ((res.data[0] & (byte) 0x01) != (byte) 0x00) {
-                scrollLog.appendData("- Allow change of master key");
-            }
-            if ((res.data[1] & (byte) 0xC0) == (byte) 0x00) {
-                scrollLog.appendData("- Crypto method = DES/3DES");
-            } else if ((res.data[1] & (byte) 0xC0) == (byte) 0x40) {
-                scrollLog.appendData("- Crypto method = 3K3DES");
-            } else if ((res.data[1] & (byte) 0xC0) == (byte) 0x80) {
-                scrollLog.appendData("- Crypto method = AES");
-            }
-            if ((res.data[1] & (byte) 0x20) != (byte) 0x00) {
-                scrollLog.appendData("- Support 2 byte file identifiers");
-            }
-            scrollLog.appendData("- No. of keys: " + ByteArray.byteToHexString((byte) (res.data[1] & (byte) 0x07)));
-
-        } catch (Exception e) {
-            Log.e("onGetKeySettings", e.getMessage(), e);
-            scrollLog.appendError("Get Key Setting  s Error");
             return null;
         }
         return res.data;
@@ -1319,97 +1272,7 @@ public class MainActivity extends AppCompatActivity implements IMainActivityCall
     //
     public void onGetFileSettingsReturn(byte bFileID) {
         setHomeStatusBar();
-
-        try {
-            Desfire.DesfireResponse res = desfireCard.getFileSettings(bFileID);
-            if (res.status != Desfire.statusType.SUCCESS) {
-                scrollLog.appendError("Get File Settings Failed: " + Desfire.DesfireErrorMsg(res.status));
-                return;
-            }
-
-            scrollLog.appendStatus("File Settings of file 0x" + ByteArray.byteToHexString(bFileID) + ": " + ByteArray.byteArrayToHexString(res.data));
-            // File Type
-            switch (res.data[0]) {
-                case (byte) 0x00:
-                    scrollLog.appendData("- Standard data file");
-                    break;
-                case (byte) 0x01:
-                    scrollLog.appendData("- Backup data file");
-                    break;
-                case (byte) 0x02:
-                    scrollLog.appendData("- Value file with backup");
-                    break;
-                case (byte) 0x03:
-                    scrollLog.appendData("- Linear record file with backup");
-                    break;
-                case (byte) 0x04:
-                    scrollLog.appendData("- Cyclic record file with backup");
-                    break;
-                default:
-                    scrollLog.appendData("- Unknown File type: 0x" + ByteArray.byteToHexString(res.data[0]));
-                    break;
-            }
-            // Comm Setting
-            switch (res.data[1]) {
-                case (byte) 0x00:
-                    scrollLog.appendData("- Plain communication");
-                    break;
-                case (byte) 0x01:
-                    scrollLog.appendData("- Plain communication secured with MAC");
-                    break;
-                case (byte) 0x03:
-                    scrollLog.appendData("- Fully enciphered communication");
-                    break;
-                default:
-                    scrollLog.appendData("- Unknown communication setting: 0x" + ByteArray.byteToHexString(res.data[1]));
-                    break;
-            }
-
-            // Separate parsing of the three types of files
-            // Standard or backup file type
-            if ((res.data[0] == (byte) 0x00) || (res.data[0] == (byte) 0x01)) {
-                parseAccessRight("Read Access         ", (byte) ((res.data[2] >> 4) & (byte) 0x0F));
-                parseAccessRight("Write Access        ", (byte) (res.data[2] & (byte) 0x0F));
-                parseAccessRight("Read & Write Access ", (byte) ((res.data[3] >> 4) & (byte) 0x0F));
-                parseAccessRight("Change Access Rights", (byte) (res.data[3] & (byte) 0x0F));
-                scrollLog.appendData("- File size           : " + ByteBuffer.wrap(res.data, 4, 3).order(ByteOrder.LITTLE_ENDIAN).getShort() + " B");
-
-            } else if ((res.data[0] == (byte) 0x02)) {  // Value File
-                parseAccessRight("GetValue/Debit Access          ", (byte) ((res.data[2] >> 4) & (byte) 0x0F));
-                parseAccessRight("GetValue/Debit/LtdCredit Access", (byte) (res.data[2] & (byte) 0x0F));
-                parseAccessRight("GetValue/Debit/LtdCredit/Credit", (byte) ((res.data[3] >> 4) & (byte) 0x0F));
-                parseAccessRight("Change Access Rights           ", (byte) (res.data[3] & (byte) 0x0F));
-                scrollLog.appendData("- Lower Limit              : " + ByteBuffer.wrap(res.data, 4, 4).order(ByteOrder.LITTLE_ENDIAN).getInt());
-                scrollLog.appendData("- Upper Limit              : " + ByteBuffer.wrap(res.data, 8, 4).order(ByteOrder.LITTLE_ENDIAN).getInt());
-                scrollLog.appendData("- Limited Credit Value     : " + ByteBuffer.wrap(res.data, 12, 4).order(ByteOrder.LITTLE_ENDIAN).getInt());
-                if ((res.data[16] & 0x01) == 1) {
-                    scrollLog.appendData("- Limited Credit enabled    ");
-                } else {
-                    scrollLog.appendData("- Limited Credit disabled   ");
-                }
-                if ((res.data[16] & 0x02) == 2) {
-                    scrollLog.appendData("- Free GetValue    ");
-                } else {
-                    scrollLog.appendData("- Free GetValue disabled   ");
-                }
-
-            } else if ((res.data[0] == (byte) 0x03) || (res.data[0] == (byte) 0x04)) {  // Record file
-                parseAccessRight("Read Access          ", (byte) ((res.data[2] >> 4) & (byte) 0x0F));
-                parseAccessRight("Write Access         ", (byte) (res.data[2] & (byte) 0x0F));
-                parseAccessRight("Read & Write Access  ", (byte) ((res.data[3] >> 4) & (byte) 0x0F));
-                parseAccessRight("Change Access Rights ", (byte) (res.data[3] & (byte) 0x0F));
-
-                scrollLog.appendData("- Record size          : " + ByteBuffer.wrap(res.data, 4, 3).order(ByteOrder.LITTLE_ENDIAN).getShort());
-                scrollLog.appendData("- Max no of records    : " + ByteBuffer.wrap(res.data, 7, 3).order(ByteOrder.LITTLE_ENDIAN).getShort());
-                scrollLog.appendData("- Current no of records: " + ByteBuffer.wrap(res.data, 10, 3).order(ByteOrder.LITTLE_ENDIAN).getShort());
-            }
-
-
-        } catch (Exception e) {
-            commandFragment.disableAllButtons();
-            scrollLog.appendError("DESFire Disconnected\n");
-            Log.e("onActivityResult", e.getMessage(), e);
-        }
+        onFragmentGetFileSettings(bFileID);
     }
 
     public Bundle onFragmentGetFileSettings(byte bFileID) {
---
+++ app/src/main/java/com/example/ac/desfirelearningtool/fChangeFileSettings.java
@@ -0,0 +1,207 @@
+package com.example.ac.desfirelearningtool;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by andrew on 2018/03/23.
+ */
+
+public class fChangeFileSettings extends Fragment {
+    View rootView;
+    IMainActivityCallbacks mCallback;
+    private Button buttonGoChangeFileSettings;
+
+    private Button buttonGetFileIDs;
+    private Spinner spFileIDs;
+    private Button buttonGetFileSettings;
+    RadioGroup rgSecondAppIndicator, rgCryptoMode, rgAddArPresent;
+    Spinner spReadAccess, spWriteAccess, spReadWriteAccess, spChangeAccessRights;
+    boolean boolTargetSecondaryApp;
+    boolean boolAddArPrsent;
+    byte bCommSetting;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        rootView = inflater.inflate(R.layout.f_changekeysettings, container, false);
+        try {
+            mCallback = (IMainActivityCallbacks) getActivity();
+            if (mCallback == null){
+                Log.d("fChangeKeySettings", "Cannot initialize callback interface");
+            }
+        } catch (ClassCastException e) {
+            throw new ClassCastException(getActivity().toString()
+                    + " must implement IMainActivityCallbacks");
+        }
+
+        spFileIDs = rootView.findViewById(R.id.spinner_FileID);
+        buttonGoChangeFileSettings =  rootView.findViewById(R.id.button_Go);
+        buttonGetFileIDs = rootView.findViewById(R.id.button_GetFiles);
+        buttonGetFileSettings = rootView.findViewById(R.id.button_GetFileSettings);
+
+        rgSecondAppIndicator = rootView.findViewById(R.id.radioGroup_SecondAppIndicator);
+        rgCryptoMode = rootView.findViewById(R.id.radioGroup_CommMode);
+        rgAddArPresent = rootView.findViewById(R.id.radioGroup_AdditionalAccessRights);
+
+        spReadAccess = (Spinner) rootView.findViewById(R.id.spinner_ReadAccess);
+        spWriteAccess = (Spinner) rootView.findViewById(R.id.spinner_WriteAccess);
+        spReadWriteAccess = (Spinner) rootView.findViewById(R.id.spinner_ReadAndWriteAccess);
+        spChangeAccessRights = (Spinner) rootView.findViewById(R.id.spinner_ChangeAccessRights);
+
+
+
+
+        populateSpinners (spFileIDs,new String[] {"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14"});
+        populateSpinners (spReadAccess,new String[] {"0","1","2","3","4","5","6","7","8","9","10","11","12","13","Free","Never"});
+        populateSpinners (spWriteAccess,new String[] {"0","1","2","3","4","5","6","7","8","9","10","11","12","13","Free","Never"});
+        populateSpinners (spReadWriteAccess,new String[] {"0","1","2","3","4","5","6","7","8","9","10","11","12","13","Free","Never"});
+        populateSpinners (spChangeAccessRights,new String[] {"0","1","2","3","4","5","6","7","8","9","10","11","12","13","Free","Never"});
+
+        rgSecondAppIndicator.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                Log.d("fChangeFileSettings:", "Radio button clicked");
+                // checkedId is the RadioButton selected
+                cdfOnRadioButtonClicked(group, checkedId);
+            }
+        });
+        rgCryptoMode.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                Log.d("fChangeFileSettings:", "Radio button clicked");
+                // checkedId is the RadioButton selected
+                cdfOnRadioButtonClicked(group, checkedId);
+            }
+        });
+        rgAddArPresent.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                Log.d("fChangeFileSettings:", "Radio button clicked");
+                // checkedId is the RadioButton selected
+                cdfOnRadioButtonClicked(group, checkedId);
+            }
+        });
+
+        buttonGoChangeFileSettings.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                buttonGoChangeFileSettings();
+            }
+        });
+
+        buttonGetFileIDs.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                onGoGetFileIDs();
+            }
+        });
+        buttonGetFileSettings.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                onGoGetFileSettings();
+            }
+        });
+
+
+
+        return rootView;
+
+    }
+
+    private void populateSpinners (Spinner targetSpinner, String[] sKeyIDs) {
+        List <String> list = new ArrayList<>();
+
+        list.add("--");
+        for (int i = 0; i < sKeyIDs.length; i++) {
+            list.add(sKeyIDs[i]);
+        }
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        targetSpinner.setAdapter(dataAdapter);
+        targetSpinner.setSelection(0);
+
+    }
+
+    public void cdfOnRadioButtonClicked(RadioGroup group, int checkedId) {
+        Log.d("cdfOnRadioButtonClicked", "initiated");
+        // Is the button now checked?
+        //boolean checked = ((RadioButton) view).isChecked();
+        //Log.d("cdfOnRadioButtonClicked", "got ischecked");
+        // Check which radio button was clicked
+        switch(checkedId) {
+            case R.id.radio_TargetPrimaryApplication:
+                boolTargetSecondaryApp = false;
+                Toast.makeText(getActivity().getApplicationContext(), "Target Primary Application", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.radio_TargetSecondaryApplication:
+                boolTargetSecondaryApp = true;
+                Toast.makeText(getActivity().getApplicationContext(), "Target Secondary Application", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.radio_AddArNotPresent:
+                boolAddArPrsent = false;
+                break;
+            case R.id.radio_AddArPresent:
+                boolAddArPrsent = true;
+                break;
+            case R.id.radio_PlainCommunication:
+                bCommSetting = (byte) 0x00;
+                break;
+            case R.id.radio_MACCommunication:
+                bCommSetting = (byte) 0x01;
+                break;
+            case R.id.radio_EncryptedCommunication:
+                bCommSetting = (byte) 0x03;
+                break;
+        }
+    }
+
+
+    private void onGoGetFileSettings () {
+
+        Log.d("fChangeFileSettings", "onGoGetFileSettings");
+
+        byte bFileID = (byte) spFileIDs.getSelectedItemId();
+        if (bFileID == (byte) 0x00) {
+
+        }
+
+        Bundle  bKeySettings = mCallback.onFragmentGetFileSettings(bFileID);
+        if (bKeySettings == null){
+            return;
+        }
+
+        spChangeKeyKey.setSelection((bKeySettings[0] >> 4)+1);
+        Log.d("onGoGetKeySettings", "Change Key Key = " + ByteArray.byteToHexString((byte)(bKeySettings[0] >> 4)));
+
+        boolAllowAMKChange = (bKeySettings[0] & 0x01) == 0x01;
+        cbMKChangeable.setChecked(boolAllowAMKChange);
+        boolFreeCreateDeleteFiles = (bKeySettings[0] & 0x02) == 0x02;
+        cbFreeCreateDelete.setChecked(boolFreeCreateDeleteFiles);
+        boolFreeDirAccess = (bKeySettings[0] & 0x04) == 0x04;
+        cbFreeDirAccess.setChecked(boolFreeDirAccess);
+        boolKeySettingsChangeable = (bKeySettings[0] & 0x08) == 0x08;
+        cbKeySettingsChangeable.setChecked(boolKeySettingsChangeable);
+
+    }
+
+    private void buttonGoChangeFileSettings(){
+
+        int iChangeKeyKey = (spChangeKeyKey.getSelectedItemPosition() - 1);
+        Log.i("onGoChangeKeySettings", "Change Key Key index" + iChangeKeyKey);
+
+        mCallback.onChangeKeySettingsReturn(iChangeKeyKey,boolAllowAMKChange, boolFreeCreateDeleteFiles, boolFreeDirAccess, boolKeySettingsChangeable);
+
+    }
+}
